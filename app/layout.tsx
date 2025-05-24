71b76bca6a8b8ce97052ff50435f6d15af092aa2
muneeb <muneebusmani8355@gmail.com>
2025-05-24 19:01:44 +0500

    feat: almost done

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@components/Header/Header";
+import Footer from "@components/Footer/Footer";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,6 +30,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
