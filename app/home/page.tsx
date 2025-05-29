bf1ed62fa33603eb9d25ddd6a536d91d06c2091c
muneebusmani <muneebusmani8355@gmail.com>
2025-05-30 03:35:27 +0500

    feat: removed

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -9,7 +9,7 @@ export default function Home(): JSX.Element {
   return (
     <div>
       <HomeHero />
-      <MenuCard />
+      {/* <MenuCard /> */}
       <HomeHero2 />
       <HomeHero3 />
       <HomeHero4 />
